311de4a60f976fcc7403dad060777faf86f5a0f7
kirtansarvadhi <kirtan.sarvadhi@gmail.com>
2026-01-19 18:49:11 +0530

    m

--- jewelry-app/src/routes/product.routes.js
+++ jewelry-app/src/routes/product.routes.js
@@ -5,12 +5,10 @@ const productController = require('../controllers/product.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const adminMiddleware = require('../middleware/admin.middleware');
 
-// Admin routes
 router.post('/', authMiddleware, adminMiddleware, productController.createProduct);
 router.put('/:id', authMiddleware, adminMiddleware, productController.updateProduct);
 router.delete('/:id', authMiddleware, adminMiddleware, productController.deleteProduct);
 
-// User + Admin routes
 router.get('/', authMiddleware, productController.getAllProducts);
 router.get('/:id', authMiddleware, productController.getProductById);
 
